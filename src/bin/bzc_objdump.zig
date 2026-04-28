8f8a42a20c6e7a8fe38de193d7af5c7dd82a68a0
Jackson Otto <jacksonotto120@gmail.com>
2026-04-27 21:11:37 -0700

    finish objdump print refactor

--- src/bin/bzc_objdump.zig
+++ src/bin/bzc_objdump.zig
@@ -42,15 +42,7 @@ const InstrPrintUtil = struct {
 
     pub fn take(self: *Self, bytes: u32) []const u8 {
         const slice = self.bytes[self.currentByte .. self.currentByte + bytes];
-        // + 1 for whitespace
-        self.currentByte += bytes + 1;
-        return slice;
-    }
-
-    pub fn takeOrLast(self: *Self, bytes: u32, isLast: bool) []const u8 {
-        const slice = self.bytes[self.currentByte .. self.currentByte + bytes];
-        // + 1 for whitespace
-        self.currentByte += bytes + if (isLast) @as(u8, 0) else @as(u8, 1);
+        self.currentByte += bytes;
         return slice;
     }
 
@@ -66,11 +58,6 @@ const InstrPrintUtil = struct {
         );
         self.currentInstrByte += @max(instr.getInstrLen(), 1);
         self.currentByte = self.currentInstrByte;
-    }
-
-    pub fn printInstr(ptr: *anyopaque, writer: *Writer) !void {
-        const self: *Self = @ptrCast(@alignCast(ptr));
-        try printBytecodeInstr(self, writer);
     }
 };
 
@@ -114,12 +101,17 @@ pub fn printBytecode(bytecode: []u8, writer: *Writer) !void {
     const numDigits: u64 = @intFromFloat(@floor(@log10(byteCountFloat)) + 1);
     const numInstrLenDigits = utils.getNumberDigitCount(u8, codegen.Instr.maxInstrSize());
 
-    const instrs = bytecode[instrStart..];
-    var printUtil = InstrPrintUtil.init(instrs);
-    var current: usize = 0;
+    const bytes = bytecode[instrStart..];
+    var printUtil = InstrPrintUtil.init(bytes);
 
-    while (printUtil.hasNext()) : (current += 1) {
-        try printBytecodeInstr(&printUtil, numDigits, numInstrLenDigits, current, writer);
+    while (printUtil.hasNext()) {
+        try printBytecodeInstr(
+            &printUtil,
+            numDigits,
+            numInstrLenDigits,
+            printUtil.currentInstr,
+            writer,
+        );
         printUtil.nextInstr();
     }
 }
@@ -165,11 +157,159 @@ fn printBytecodeInstr(
     try writer.writeByte(' ');
 
     switch (instr) {
+        .MovSpNegOffsetAny,
+        .PrePushRegNegOffsetAny,
+        .PostPopRegNegOffsetAny,
+        .PrePushLRNegOffsetAny,
+        .PostPopLRNegOffsetAny,
+        => unreachable,
+
         .Label, .NoOp, .Ret, .End => {},
-        .SetReg64 => {
-            try printSegments(printUtil, .{ .Reg, .Immediate64 }, writer);
-        },
-        else => {},
+
+        .SetReg64 => try printSegments(printUtil, .{ .Reg, .Immediate64 }, writer),
+        .SetReg32 => try printSegments(printUtil, .{ .Reg, .Immediate32 }, writer),
+        .SetReg16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .SetReg8 => try printSegments(printUtil, .{ .Reg, .Immediate8 }, writer),
+
+        .Add => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .Sub => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .Mult => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+
+        .Add8 => try printSegments(printUtil, .{ .Reg, .Reg, .Immediate8 }, writer),
+        .Sub8 => try printSegments(printUtil, .{ .Reg, .Reg, .Immediate8 }, writer),
+
+        .Add16 => try printSegments(printUtil, .{ .Reg, .Reg, .Immediate16 }, writer),
+        .Sub16 => try printSegments(printUtil, .{ .Reg, .Reg, .Immediate16 }, writer),
+
+        .Jump => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpEQ => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpNE => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpGT => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpLT => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpGTE => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpLTE => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpBack => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpBackEQ => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpBackNE => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpBackGT => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpBackLT => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpBackGTE => try printSegments(printUtil, .{.Immediate32}, writer),
+        .JumpBackLTE => try printSegments(printUtil, .{.Immediate32}, writer),
+
+        .Cmp => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .CmpSetRegEQ => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .CmpSetRegNE => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .CmpSetRegGT => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .CmpSetRegLT => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .CmpSetRegGTE => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .CmpSetRegLTE => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .CmpConst8 => try printSegments(printUtil, .{ .Reg, .Immediate8 }, writer),
+
+        .IncConst8 => try printSegments(printUtil, .{ .Reg, .Immediate8 }, writer),
+        .DecConst8 => try printSegments(printUtil, .{ .Reg, .Immediate8 }, writer),
+
+        .Mov => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .MovSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .MovSpNegOffset32 => try printSegments(printUtil, .{ .Reg, .Immediate32 }, writer),
+        .MovSpNegOffset64 => try printSegments(printUtil, .{ .Reg, .Immediate64 }, writer),
+
+        .Xor => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .XorConst8 => try printSegments(printUtil, .{ .Reg, .Reg, .Immediate8 }, writer),
+
+        .AddSp8 => try printSegments(printUtil, .{.Immediate8}, writer),
+        .SubSp8 => try printSegments(printUtil, .{.Immediate8}, writer),
+        .AddSp16 => try printSegments(printUtil, .{.Immediate16}, writer),
+        .SubSp16 => try printSegments(printUtil, .{.Immediate16}, writer),
+        .AddSp32 => try printSegments(printUtil, .{.Immediate32}, writer),
+        .SubSp32 => try printSegments(printUtil, .{.Immediate32}, writer),
+        .AddSp64 => try printSegments(printUtil, .{.Immediate64}, writer),
+        .SubSp64 => try printSegments(printUtil, .{.Immediate64}, writer),
+
+        .Store64AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .Store32AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .Store16AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .Store8AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+
+        .Store64AtRegPostInc16 => try printSegments(
+            printUtil,
+            .{ .Reg, .Reg, .Immediate16 },
+            writer,
+        ),
+        .Store32AtRegPostInc16 => try printSegments(
+            printUtil,
+            .{ .Reg, .Reg, .Immediate16 },
+            writer,
+        ),
+        .Store16AtRegPostInc16 => try printSegments(
+            printUtil,
+            .{ .Reg, .Reg, .Immediate16 },
+            writer,
+        ),
+        .Store8AtRegPostInc16 => try printSegments(
+            printUtil,
+            .{ .Reg, .Reg, .Immediate16 },
+            writer,
+        ),
+
+        .Store64AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Store32AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Store16AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Store8AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+
+        .Load64AtRegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Load32AtRegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Load16AtRegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Load8AtRegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+
+        .Load64AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Load32AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Load16AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .Load8AtSpNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+
+        .Load64AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .Load32AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .Load16AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .Load8AtReg => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+
+        .MulReg16AddReg => try printSegments(
+            printUtil,
+            .{ .Reg, .Reg, .Reg, .Immediate16 },
+            writer,
+        ),
+
+        .DbgReg => try printSegments(printUtil, .{.Reg}, writer),
+
+        .BitAnd => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .BitOr => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+
+        .And => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+        .Or => try printSegments(printUtil, .{ .Reg, .Reg }, writer),
+
+        .AndSetReg => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+        .OrSetReg => try printSegments(printUtil, .{ .Reg, .Reg, .Reg }, writer),
+
+        .PrePushRegNegOffset8 => try printSegments(printUtil, .{ .Reg, .Immediate8 }, writer),
+        .PrePushRegNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .PrePushRegNegOffset32 => try printSegments(printUtil, .{ .Reg, .Immediate32 }, writer),
+        .PrePushRegNegOffset64 => try printSegments(printUtil, .{ .Reg, .Immediate64 }, writer),
+
+        .PostPopRegNegOffset8 => try printSegments(printUtil, .{ .Reg, .Immediate8 }, writer),
+        .PostPopRegNegOffset16 => try printSegments(printUtil, .{ .Reg, .Immediate16 }, writer),
+        .PostPopRegNegOffset32 => try printSegments(printUtil, .{ .Reg, .Immediate32 }, writer),
+        .PostPopRegNegOffset64 => try printSegments(printUtil, .{ .Reg, .Immediate64 }, writer),
+
+        .PrePushLRNegOffset8 => try printSegments(printUtil, .{.Immediate8}, writer),
+        .PrePushLRNegOffset16 => try printSegments(printUtil, .{.Immediate16}, writer),
+        .PrePushLRNegOffset32 => try printSegments(printUtil, .{.Immediate32}, writer),
+        .PrePushLRNegOffset64 => try printSegments(printUtil, .{.Immediate64}, writer),
+
+        .PostPopLRNegOffset8 => try printSegments(printUtil, .{.Immediate8}, writer),
+        .PostPopLRNegOffset16 => try printSegments(printUtil, .{.Immediate16}, writer),
+        .PostPopLRNegOffset32 => try printSegments(printUtil, .{.Immediate32}, writer),
+        .PostPopLRNegOffset64 => try printSegments(printUtil, .{.Immediate64}, writer),
+
+        .BranchLink => try printSegments(printUtil, .{.Immediate32}, writer),
+        .BranchLinkBack => try printSegments(printUtil, .{.Immediate32}, writer),
     }
 
     try writer.writeByte('\n');
@@ -180,29 +320,27 @@ fn printSegments(
     comptime segments: anytype,
     writer: *Writer,
 ) !void {
-    inline for (segments, 0..) |segment, index| {
-        const isLast = index + 1 == segments.len;
-
+    inline for (segments) |segment| {
         switch (segment) {
             .Reg => {
-                const regByte = printUtil.takeOrLast(1, isLast)[0];
+                const regByte = printUtil.take(1)[0];
                 try writer.writeByte('r');
                 try writer.printInt(regByte, 10, .lower, .{});
             },
             .Immediate8 => {
-                const str = printUtil.takeOrLast(1, isLast)[0..];
+                const str = printUtil.take(1);
                 try formatHexDecNumber(u8, str, writer);
             },
             .Immediate16 => {
-                const str = printUtil.takeOrLast(2, isLast)[0..];
+                const str = printUtil.take(2);
                 try formatHexDecNumber(u16, str, writer);
             },
             .Immediate32 => {
-                const str = printUtil.takeOrLast(4, isLast)[0..];
+                const str = printUtil.take(4);
                 try formatHexDecNumber(u32, str, writer);
             },
             .Immediate64 => {
-                const str = printUtil.takeOrLast(8, isLast)[0..];
+                const str = printUtil.take(8);
                 try formatHexDecNumber(u64, str, writer);
             },
             else => @compileError("Unexpected enum"),
